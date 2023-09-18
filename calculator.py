2b694ce891cf976df128729d40cc9ddc64546cf6
raissa ecker <145071876+raissaecker@users.noreply.github.com>
2023-09-18 20:08:19 -0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,48 @@
+def adicao(n1: int, n2: int) -> int:
+    """Soma dois números.
+
+    Args:
+        n1: o primeiro número.
+        n2: o segundo número.
+
+    Returns:
+        A soma dos dois números.
+    """
+
+    return n1 + n2
+
+def multiplicacao(n1: int, n2: int) -> int:
+    """Realiza a multiplicação entre dois números.
+
+    Args:
+        n1: o primeiro número.
+        n2: o segundo número.
+
+    Returns:
+        O produto entre dois números.
+    """
+    return n1 * n2
+
+def subtracao(n1: int, n2: int) -> int:
+    """Realiza a subtração de dois números.
+
+    Args:
+        n1: o primeiro número.
+        n2: o segundo número.
+
+    Returns:
+        A subtração dos dois números.
+    """
+    return n1 - n2
+
+def divisao(n1: int, n2: int) -> int:
+    """Realiza a divisão de dois números.
+
+    Args:
+        n1: o primeiro número.
+        n2: o segundo número.
+
+    Returns:
+        O quociente de dois números.
+    """
+    return n1 / n2
